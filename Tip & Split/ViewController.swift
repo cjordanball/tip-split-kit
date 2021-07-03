3e87fef5eec64738da7121013b140800ca4bfd58
Jordan Ball <cjordanball@ballwebdev.com>
2021-07-03 14:48:21 -0400

    Initial Commit

---
+++ Tip & Split/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tip & Split
+//
+//  Created by C. Jordan Ball III on 7/3/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
